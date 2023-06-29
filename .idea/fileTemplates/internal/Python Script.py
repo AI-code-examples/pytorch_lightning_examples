1f05fa5bb6b60989a5b24d61b3cc625a924f50e2
zYx.Tom <ygpfr74@hotmail.com>
2023-06-29 15:54:53 +0800

    Create(idea): PyCharm的配置文件

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#  -*- encoding: utf-8 -*-
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+-----------------------------------------------
+@Software   :   ${PRODUCT_NAME}
+@Project    :   ${PROJECT_NAME}
+@File       :   ${NAME}
+@Version    :   v0.1
+@Time       :   ${DATE} ${TIME}
+@License    :   (C)Copyright    2018-2023,  zYx.Tom
+@Reference  :   
+@Description:   
+@Thought    :
+"""
+import os
+import torch
+from torch import optim,nn,utils,Tensor
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import lightning.pytorch as pl
+
+
+def main():
+    pass
+
+
+# ----------------------------------------------------------------------
+# 小结
+if __name__ == '__main__':
+    main()
